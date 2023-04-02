71a0cb1013bcc6cec1b6c2812daa946c51bbc54b
Demetrio <demetrio.teodoro@gmail.com>
2023-04-02 00:54:05 -0300

    Arquivo para rodar testes

---
+++ src/test/java/com/br/salesmanagement/MoneyWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.salesmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
