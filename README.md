2f3071e70185609c1b9dcd5d1efd6d324e7778f6
Demetrio <demetrio.teodoro@gmail.com>
2023-04-02 00:49:12 -0300

    Criado projeto para gerenciamento de vendas

---
+++ README.md
@@ -0,0 +1,11 @@
+## Project
+- Money Worker
+
+## Author
+
+- [Demetrio Teodoro](https://github.com/DemetrioTeodoro)
+
+## License
+
+- MIT licensed
+
---
+++ src/main/java/br/com/moneyworker/MoneyWorkerApplication.java
@@ -0,0 +1,13 @@
+package br.com.moneyworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyWorkerApplication.class, args);
+	}
+
+}
