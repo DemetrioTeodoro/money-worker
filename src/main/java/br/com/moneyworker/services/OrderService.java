0335d9cc8144234beddc51e45c683a5c7fc17a82
Demetrio <demetrio.teodoro@gmail.com>
2023-04-03 21:29:17 -0300

    Criado service order

---
+++ src/main/java/br/com/moneyworker/services/OrderService.java
@@ -0,0 +1,26 @@
+package br.com.moneyworker.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.moneyworker.entities.Order;
+import br.com.moneyworker.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository orderRepository;
+
+	public List<Order> findAll() {
+		return orderRepository.findAll();
+	}
+
+	public Order findById(Long id) {
+		Optional<Order> userOptional = orderRepository.findById(id);
+		return userOptional.get();
+	}
+}
