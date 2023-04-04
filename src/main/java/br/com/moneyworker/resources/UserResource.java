41b6bf094d64bc1f0ec07c98d2590f5251486ed0
Demetrio <demetrio.teodoro@gmail.com>
2023-04-03 21:32:00 -0300

    Adicionado endpoint com id

--- src/main/java/br/com/moneyworker/resources/UserResource.java
+++ src/main/java/br/com/moneyworker/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +15,17 @@ import br.com.moneyworker.services.UserService;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
 		return ResponseEntity.ok().body(userService.findAll());
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		return ResponseEntity.ok().body(userService.findById(id));
 	}
 }
