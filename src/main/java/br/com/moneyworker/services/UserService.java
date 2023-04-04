86b08f7f823e21b24890fcc76b229134211c62d0
Demetrio <demetrio.teodoro@gmail.com>
2023-04-03 21:33:11 -0300

    Adicionado endpoint com id

--- src/main/java/br/com/moneyworker/services/UserService.java
+++ src/main/java/br/com/moneyworker/services/UserService.java
@@ -1,6 +1,7 @@
 package br.com.moneyworker.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,11 +11,16 @@ import br.com.moneyworker.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
 
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> userOptional = userRepository.findById(id);
+		return userOptional.get();
 	}
 }
