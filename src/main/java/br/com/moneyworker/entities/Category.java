1b5472da579438e72207e1186458a0ca5873b338
Demetrio <demetrio.teodoro@gmail.com>
2023-04-06 23:49:21 -0300

    fix: Adicionado relacionaento entre product e category

--- src/main/java/br/com/moneyworker/entities/Category.java
+++ src/main/java/br/com/moneyworker/entities/Category.java
@@ -1,23 +1,29 @@
 package br.com.moneyworker.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
+
+	@Transient
+	private Set<Product> products = new HashSet<>();
+
 	public Category(Long id, String name) {
 		super();
 		this.id = id;
@@ -42,6 +48,10 @@ public class Category implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@Override
@@ -69,5 +79,4 @@ public class Category implements Serializable {
 		return true;
 	}
 
-	
 }
