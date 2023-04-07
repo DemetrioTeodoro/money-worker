9c441b930ce35b5da885c378c15dc3e2a86d33f1
Demetrio <demetrio.teodoro@gmail.com>
2023-04-07 14:01:36 -0300

    feat: Adicionado relacionamento de categories e products no banco de testes

--- src/main/java/br/com/moneyworker/config/TestConfig.java
+++ src/main/java/br/com/moneyworker/config/TestConfig.java
@@ -27,10 +27,10 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private OrderRepository orderRepository;
-	
+
 	@Autowired
 	private CategoryRepository categoryRepository;
-	
+
 	@Autowired
 	private ProductRepository productRepository;
 
@@ -39,11 +39,11 @@ public class TestConfig implements CommandLineRunner {
 		Category category1 = new Category(null, "Clothes");
 		Category category2 = new Category(null, "Sporting");
 		Category category3 = new Category(null, "Technological");
-		
+
 		Product product1 = new Product(null, "Cadeira gamer", "Cadeira confortavel e ergnomica", 990.9, "");
 		Product product2 = new Product(null, "PC gamer", "Pc gamer com placa de video dedicada", 3500.5, "");
 		Product product3 = new Product(null, "Bicicleta mountain bike", "Bicicleta aro 29 com amortecedor", 2500.5, "");
-		Product product4 = new Product(null, "Notebook ultrafino e super potente", "Notebook ultra fino e com procesador i7", 4500.5, "");
+		Product product4 = new Product(null, "Camiseta", "Camiseta de algodão 100%", 4500.5, "");
 		Product product5 = new Product(null, "Raquete de badmington", "Raquete de badmington leve e potente", 500.5, "");
 
 		User user1 = new User(null, "Maria", "maria@gmail.com", "988888888", "123456");
@@ -55,6 +55,15 @@ public class TestConfig implements CommandLineRunner {
 
 		categoryRepository.saveAll(Arrays.asList(category1, category2, category3));
 		productRepository.saveAll(Arrays.asList(product1, product2, product3, product4, product5));
+
+		product1.getCategories().add(category3);
+		product2.getCategories().add(category3);
+		product3.getCategories().add(category2);
+		product4.getCategories().add(category1);
+		product5.getCategories().add(category2);
+
+		productRepository.saveAll(Arrays.asList(product1, product2, product3, product4, product5));
+
 		userRepository.saveAll(Arrays.asList(user1, user2));
 		orderRepository.saveAll(Arrays.asList(order1, order2, order3));
 	}
