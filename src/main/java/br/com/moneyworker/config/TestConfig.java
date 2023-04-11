a7eccd071133ed63dfdc59d0d7d5c1b89a7c7f29
Demetrio <demetrio.teodoro@gmail.com>
2023-04-10 23:30:00 -0300

    feat: Adicionado payment para popular o banco de testes

--- src/main/java/br/com/moneyworker/config/TestConfig.java
+++ src/main/java/br/com/moneyworker/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.moneyworker.entities.Category;
 import br.com.moneyworker.entities.Order;
 import br.com.moneyworker.entities.OrderItem;
+import br.com.moneyworker.entities.Payment;
 import br.com.moneyworker.entities.Product;
 import br.com.moneyworker.entities.User;
 import br.com.moneyworker.entities.enums.OrderStatus;
@@ -35,7 +36,7 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private ProductRepository productRepository;
-	
+
 	@Autowired
 	private OrderItemRepository orderItemRepository;
 
@@ -54,9 +55,9 @@ public class TestConfig implements CommandLineRunner {
 		User user1 = new User(null, "Maria", "maria@gmail.com", "988888888", "123456");
 		User user2 = new User(null, "Arthur", "arthur@gmail.com", "977777777", "123456");
 
-		Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, user1);
+		Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, user1);
 		Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.DELIVERED, user2);
-		Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, user2);
+		Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, user2);
 
 		categoryRepository.saveAll(Arrays.asList(category1, category2, category3));
 		productRepository.saveAll(Arrays.asList(product1, product2, product3, product4, product5));
@@ -78,5 +79,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem orderItem4 = new OrderItem(order3, product5, 3, product5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(orderItem1, orderItem2, orderItem3, orderItem4));
+
+		Payment payment1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), order1);
+
+		order1.setPayment(payment1);
+
+		orderRepository.save(order1);
 	}
 }
