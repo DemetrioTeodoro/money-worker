49887cce05d377c79cc057fbc36d1de9ec437112
Demetrio <demetrio.teodoro@gmail.com>
2023-04-14 11:30:24 -0300

    feat: Criado endpoint para salvar user

--- src/main/java/br/com/moneyworker/resources/UserResource.java
+++ src/main/java/br/com/moneyworker/resources/UserResource.java
@@ -1,13 +1,17 @@
 package br.com.moneyworker.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.moneyworker.entities.User;
 import br.com.moneyworker.services.UserService;
@@ -27,5 +31,12 @@ public class UserResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		return ResponseEntity.ok().body(userService.findById(id));
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> saveUser(@RequestBody User user) {
+		user = userService.saveUser(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).body(user);
 	}
 }
